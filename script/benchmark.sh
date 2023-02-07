7207035c9d54cc1b19bd882b5266b1abe2c472d4
kinten108101 <kinten108101@protonmail.com>
2023-02-07 18:16:47 +0700

    Move demo scripts out of this repo

--- script/benchmark.sh
+++
@@ -1,2 +0,0 @@
-echo -e "Running: extract \"kmod.vpk\" with bash script addons_manager"
-echo -e "Running: extract \"kmod.vpk\" with binary addonsman"
--- script/matesvpk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. "$(cd ${0%/*} && echo ${PWD})/init" _INTERNAL
-
-case $1 in
-	(download) shift; exec "${BINDIR}/workshop_manager" $@;;
-	(addons) shift; exec "${BINDIR}/addons_manager" $@;;
-	(game) shift; exec "${BINDIR}/game_manager" $@;;
-	(*) ulog "Unimplemented option in matesvpk" 'E';;
-esac
